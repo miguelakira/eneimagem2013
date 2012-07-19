a98f32b3c58e3d069736e67f76268f536242aab5
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-07-19 17:42:15 -0300

    adicionado novo contador

---
+++ eixos.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+<?php include "header.php"; ?>
+<body>
+<div id="container">
+  <?php include "logo.php"; ?>
+  <?php include "top-menu.php"; ?>
+
+  <?php include "menu.php"; ?>  
+    
+  <div id="content" class = "rounded-corners">
+
+<div id = "justify">
+  <h1><p>Eixos Temáticos</p></h1>
+  <hr>
+
+<table>
+<tr>
+<th>Num.</th>
+<th>TITULO</th>
+<th>COORDENADOR (A)</th>
+</tr>
+<tr>
+<td>1</td>
+<td>Álbum de família</td> 
+<td>Alberto Gawryszewski</td>
+
+<tr class = "alt">
+<td>2</td>
+<td>Imagens da morte</td>
+<td>Alberto Gawryszewski</td>
+</tr>
+
+<tr>
+<td>3</td>
+<td>Imagem e arte</td> 
+<td>Ana  Heloísa Molina</td>
+</tr>
+
+<tr class = "alt">
+<td>4</td>
+<td>Imagem e religião</td> 
+<td>Angelita Marques Visalli</td>
+</tr>
+
+<tr>
+<td>5</td>
+<td>Cultura material e imagem </td>
+<td>Cláudia Martinez</td>
+</tr>
+
+<tr class = "alt">
+<td>6</td>
+<td>Gênero em imagens</td>  
+<td>Edméia Ribeiro</td>
+</tr>
+
+<tr>
+<td>7</td>
+<td>Pesquisa em imagens: debates teóricos</td> 
+<td>Edméia Ribeiro</td>
+</tr>
+
+<tr class = "alt">
+<td>8</td>
+<td>Semiótica</td> 
+<td>Luiz Carlos Migliozzi</td>
+</tr>
+
+<tr>
+<td>9</td>
+<td>Literatura e Imagem</td> 
+<td>Márcia Rorato</td>
+</tr>
+
+<tr class = "alt">
+<td>10</td>
+<td>História e cinema</td> 
+<td>Márcia Rorato</td>
+</tr>
+
+<tr>
+<td>11</td>
+<td>Imagem política</td> 
+<td>Paulo Alves</td>
+</tr>
+
+<tr class = "alt">
+<td>12</td>
+<td>História e imprensa</td> 
+<td>Paulo Boni</td>
+</tr>
+
+<tr>
+<td>13</td>
+<td>Fotografia e fotorreportagem</td> 
+<td>Paulo Boni</td>
+</tr>
+
+<tr class = "alt">
+<td>14</td>
+<td>Humor gráfico</td> 
+<td>Rozinaldo Miani</td>
+</tr>
+
+<tr>
+<td>15</td>
+<td>Educação e Imagem</td>
+<td>Terezinha Oliveira</td>
+</tr>
+
+<tr class = "alt">
+<td>16</td>
+<td>Patrimônio Cultural e Imagem</td>
+<td>Zueleide Casagrande de Paula</td>
+</tr>
+
+<tr>
+<td>17</td>
+<td>Imagens Urbanas</td> 
+<td>Zueleide Casagrande de Paula</td>
+</tr>
+
+</table>
+
+
+
+</div>
+</div>
+</div>
+<div id="footer" class = "bottom-rounded-corners">
+      LEDI© Todos os Direitos Reservados.
+</div>
+</body>
+</html>
+
--- menu.php
+++ menu.php
@@ -7,16 +7,17 @@
       <li><a href="transmissao.php">Transmissão Online</a></li>
       <li><a href="hosppass.php">Hospedagem</a></li>
       <li><a href="inscricao.php">Inscrição</a></li>
+      <li><a href="eixos.php">Eixos Temáticos</a></li>
     </ul>
     <div id = "counter">
-      <!-- Histats.com  START  (standard)-->
+    <!-- Histats.com  START  (standard)-->
       <script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
-      <a href="http://www.histats.com" target="_blank" title="hit counter code" ><script  type="text/javascript" >
-      try {Histats.start(1,1284363,4,139,85,17,"00001000");
+      <a href="http://www.histats.com" target="_blank" title="free log" ><script  type="text/javascript" >
+      try {Histats.start(1,2005431,4,137,112,33,"00000010");
       Histats.track_hits();} catch(err){};
       </script></a>
-      <noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?1284363&101" alt="hit counter code" border="0"></a></noscript>
-      <!-- Histats.com  END  -->
+      <noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?2005431&101" alt="free log" border="0"></a></noscript>
+    <!-- Histats.com  END  -->
     </div>
 </div>
   
